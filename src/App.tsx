11ced01e977cb6b7ab2fe3084844ec643afab6c8
Shayla White <derekwhite.m@gmail.com>
2023-12-27 14:16:13 -0500

    implement shadcn themeing

--- src/App.tsx
+++ src/App.tsx
@@ -11,22 +11,26 @@ function App() {
     const game = new Game("ball", scoreboard);
     return (
         <>
-            <div className="bg-[#1b1b1b] max-w-[16rem] shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]">
-                <h1 className="text-[1.8rem] text-center m-auto p-4 text-white">Serve Spotter</h1>
+            <div className="max-w-[16rem]">
+                <h1 className="text-[1.8rem] text-center m-auto p-4">Serve Spotter</h1>
                 <div className="flex flex-col">
                     <div>
-                        <h2 className="text-center bg-[#292929] py-[0.236rem] text-white">Guide</h2>
-                        <div className="px-4 py-6 text-white">
-                            Click on the left or right side of the court (arrow keys work too) to
-                            return each serve as quicly as you can.
+                        <h2 className="text-center bg-secondary text-secondary-foreground py-[0.236rem]">Guide</h2>
+                        <div className="px-4 py-6">
+                            Use the left and right arrow keys to return each serve as quickly as you
+                            can.
                         </div>
                     </div>
                     <div>
-                        <h2 className="text-center bg-[#292929] py-[0.236rem] text-white">Configuration</h2>
+                        <h2 className="text-center bg-secondary text-secondary-foreground py-[0.236rem]">
+                            Configuration
+                        </h2>
                         <div className="px-4 py-6">
-                            <ul className="flex flex-col gap-[0.236rem] mb-4">
+                            <ul className="flex flex-col gap-[0.382rem] mb-4">
                                 <li className="flex justify-between items-center gap-[0.236rem]">
-                                    <Label htmlFor="speed" className="text-white">Speed (1-10)</Label>
+                                    <Label htmlFor="speed">
+                                        Speed (1-10)
+                                    </Label>
                                     <Input
                                         name="speed"
                                         type="number"
@@ -34,10 +38,13 @@ function App() {
                                         defaultValue={5}
                                         min={1}
                                         max={10}
+                                        className="text-primary"
                                     />
                                 </li>
                                 <li className="flex justify-between items-center gap-[0.236rem]">
-                                    <Label htmlFor="frequency" className="text-white">Frequency (ms)</Label>
+                                    <Label htmlFor="frequency">
+                                        Frequency (ms)
+                                    </Label>
                                     <Input
                                         name="frequency"
                                         type="number"
@@ -45,10 +52,13 @@ function App() {
                                         defaultValue={1000}
                                         min={100}
                                         max={10000}
+                                        className="text-primary"
                                     />
                                 </li>
                                 <li className="flex justify-between items-center gap-[0.236rem]">
-                                    <Label htmlFor="rounds" className="text-white">Rounds (1-10)</Label>
+                                    <Label htmlFor="rounds">
+                                        Rounds (1-10)
+                                    </Label>
                                     <Input
                                         name="rounds"
                                         type="number"
@@ -56,11 +66,12 @@ function App() {
                                         defaultValue={3}
                                         min={1}
                                         max={10}
+                                        className="text-primary"
                                     />
                                 </li>
                             </ul>
                             <Button
-                                className="w-full mt-[0.236rem] text-white"
+                                className="bg-primary w-full mt-[0.236rem]"
                                 onClick={() => {
                                     const speed = Number(getInputElementByName("speed").value);
                                     const frequency = Number(
@@ -76,42 +87,57 @@ function App() {
                             >
                                 Start
                             </Button>
-                            <Button className="w-full mt-[0.236rem]" variant="secondary" onClick={() => game.reset()}>
+                            <Button
+                                className="w-full mt-[0.236rem] border bg-background shadow-sm hover:bg-accent hover:text-accent-foreground"
+                                variant="secondary"
+                                onClick={() => game.reset()}
+                            >
                                 Reset
                             </Button>
                         </div>
                     </div>
                     <div>
-                        <h2 className="text-center bg-[#292929] py-[0.236rem] text-white">Scoreboard</h2>
+                        <h2 className="text-center bg-secondary text-secondary-foreground py-[0.236rem]">
+                            Scoreboard
+                        </h2>
                         <div className="px-4 py-6">
-                            <table id="scoreboard" className="w-full text-center text-white">
+                            <table id="scoreboard" className="w-full text-center">
                                 <thead>
-                                    <th>Round</th>
-                                    <th>Score</th>
+                                    <tr>
+                                        <th>Round</th>
+                                        <th>Score</th>
+                                    </tr>
                                 </thead>
                                 <tbody></tbody>
                             </table>
-                            <div id="avg" className="w-full text-center font-bold mt-4 text-white"></div>
+                            <div
+                                id="avg"
+                                className="w-full text-center font-bold mt-4"
+                            ></div>
                         </div>
                     </div>
                 </div>
             </div>
-
-            <div className="w-[425px] h-[850px] border relative m-auto border-solid border-[white]">
-                <div id="ball" className="absolute h-5 w-5 bg-[#FFD700] m-auto rounded-[10px] left-[202px] -top-2.5"></div>
-                <div className="flex flex-col h-full">
-                    <div className="h-full w-full"></div>
-                    <div className="flex h-full w-full border-y-[white] border-t border-solid border-b">
-                        <div className="h-full w-full border-r-[white] border-r border-solid"></div>
+            <div className="bg-primary w-full flex">
+                <div className="w-[425px] h-[850px] border relative m-auto border-solid border-[white]">
+                    <div
+                        id="ball"
+                        className="absolute h-5 w-5 bg-[#FFD700] m-auto rounded-[10px] left-[202px] -top-2.5"
+                    ></div>
+                    <div className="flex flex-col h-full">
                         <div className="h-full w-full"></div>
-                    </div>
-                    <div className="flex h-full w-full border-y-[white] border-t border-solid border-b">
-                        <div className="h-full w-full border-r-[white] border-r border-solid"></div>
-                        <div className="h-full w-full"></div>
-                    </div>
-                    <div className="flex h-full w-full">
-                        <div className="h-full w-full" onClick={() => game.return("ad")}></div>
-                        <div className="h-full w-full" onClick={() => game.return("deuce")}></div>
+                        <div className="flex h-full w-full border-y-[white] border-t border-solid border-b">
+                            <div className="h-full w-full border-r-[white] border-r border-solid"></div>
+                            <div className="h-full w-full"></div>
+                        </div>
+                        <div className="flex h-full w-full border-y-[white] border-t border-solid border-b">
+                            <div className="h-full w-full border-r-[white] border-r border-solid"></div>
+                            <div className="h-full w-full"></div>
+                        </div>
+                        <div className="flex h-full w-full">
+                            <div className="h-full w-full"></div>
+                            <div className="h-full w-full"></div>
+                        </div>
                     </div>
                 </div>
             </div>
