3edced4da841504d5e740d563c3b0eb58bf7a873
Shayla White <derekwhite.m@gmail.com>
2023-12-31 10:11:10 -0500

    set dynamic host url for prod vs dev

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,5 +1,8 @@
 import PocketBase from "pocketbase";
 
-const url = "https://serve-spotter.fly.dev";
+const url =
+    process.env.NODE_ENV === "production"
+        ? "https://serve-spotter.fly.dev"
+        : "http://localhost:8090";
 
 export const pb = new PocketBase(url);
